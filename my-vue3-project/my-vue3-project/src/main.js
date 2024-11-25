c837dd95fb94bf79b961cdfc838e186f973537cc
赵彦涵 <3435533971@qq.com>
2024-11-25 17:44:45 +0800

    更新上方导航栏topbar（可直接引用）和个人主页personalpage

--- my-vue3-project/my-vue3-project/src/main.js
+++ my-vue3-project/my-vue3-project/src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import ElementPlus from 'element-plus' // 导入ElementPlus
 import router from './router';
 
 const app = createApp(App);
-
 app.use(router);
-
+app.use(ElementPlus); // 使用Element UI
 app.mount('#app');
