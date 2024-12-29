58256177ded1422725a96eba29ae7a028246f2fb
JaxxyH <z913510640@163.com>
2024-12-30 00:31:26 +0800

    增加创建聊天室页面

--- Partner_Server/main.go
+++ Partner_Server/main.go
@@ -21,7 +21,8 @@ import (
 
 func main() {
 	//定义Redis服务器路径
-	redisServerPath := "../Redis-x64-3.2.100/redis-server.exe"
+	// redisServerPath := "../Redis-x64-3.2.100/redis-server.exe"
+	redisServerPath := "/opt/homebrew/bin/redis-server" //mac系统
 
 	//启动Redis服务器
 	redisServerCmd := exec.Command(redisServerPath)
@@ -58,7 +59,8 @@ func main() {
 	time.Sleep(2 * time.Second)
 
 	//定义Redis客户端路径
-	redisClientPath := "../Redis-x64-3.2.100/redis-cli.exe"
+	// redisClientPath := "../Redis-x64-3.2.100/redis-cli.exe"
+	redisClientPath := "/opt/homebrew/bin/redis-cli"
 	redisClientCmd := exec.Command(redisClientPath, "ping")
 	redisClientCmd.Stdout = os.Stdout
 	redisClientCmd.Stderr = os.Stderr
